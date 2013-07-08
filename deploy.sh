6fc54a7945dad2aa3193b06f3b29a9d6bad13216
Nick HS <nickhs@umich.edu>
2013-07-08 14:45:34 -0700

    Add in fast deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
-ansible-playbook ops/deploy.yml -i ops/hosts --private-key=$HOME/Downloads/main.pem
+str="ansible-playbook ops/deploy.yml -i ops/hosts"
+
+if [[ $1 = "--fast" ]]; then
+    echo "Performing fast deploy"
+    str="${str} --extra-vars "fast_deploy=true""
+elif [[ $1 = "--help" ]]; then
+    echo "Usage: deploy.sh [deploy-type]"
+    echo "  --fast    Do a fast deploy where only the code is updated and the app is restarted"
+    echo "  --help    This message"
+    exit
+else
+    echo "Performing normal deploy"
+fi
+
+exec $str
