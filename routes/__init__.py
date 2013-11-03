56b307a0f07700a13c88128d34b2524a407d86a0
Satrajit Ghosh <satra@mit.edu>
2013-11-03 01:17:39 -0500

    api: added basic paths

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,12 @@
-from site import home, about, send_text_file
+from site import (home, about, send_text_file, submit_job, destroy_job,
+                  job_status, get_file)
 
 __all__ = [
     'home',
     'about',
-    'send_text_file'
+    'send_text_file',
+    'submit_job',
+    'destroy_job',
+    'job_status',
+    'get_file'
 ]
--- routes/site.py
+++ routes/site.py
@@ -2,6 +2,7 @@ from app import app
 
 from flask import render_template
 
+from uuid import uuid1
 
 @app.route('/')
 def home():
@@ -20,3 +21,30 @@ def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
+
+@app.route('/submit', methods=['POST'])
+def submit_job():
+    """Submit a job to the queue."""
+    id = uuid1().hex
+    app.jobs.append(id)
+    print app.jobs
+    return "Submitting job[%s]" % id
+
+@app.route('/destroy/<id>')
+def destroy_job(id):
+    """Remove a job from the queue."""
+    try:
+        app.jobs.remove(id)
+        return 'Removing job[%s] from queue' % id
+    except Exception, e:
+        return 'Could not remove job[%s] because %s' % (id, e)
+
+@app.route('/status/<id>')
+def job_status(id):
+    """Return current status of job from queue."""
+    return 'job: %s status: unknown ' % id
+
+@app.route('/file/<location>')
+def get_file(location):
+    """Return current status of job from queue."""
+    return 'location: %s ' % location
